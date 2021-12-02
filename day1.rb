b2772e0ea57bb946c571b10618004c0e63a7dfd3
dcromer <cromer.dan@gmail.com>
2021-12-01 22:30:26 -0800

    Day 1

---
+++ day1.rb
@@ -0,0 +1,15 @@
+def assert_equal(a, b)
+    raise "Expected #{a} == #{b}" unless a == b
+end
+
+input = File.open("input/day1.txt").readlines.map(&:strip).reject(&:empty?).map(&:to_i)
+
+def num_increasing(array)
+    array.each_cons(2).select { |(a, b)| a < b }.count
+end
+
+assert_equal 1466, num_increasing(input)
+
+part_2_input = input.each_cons(3).map(&:sum)
+
+assert_equal 1491, num_increasing(part_2_input)
