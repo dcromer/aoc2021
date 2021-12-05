bf1be0ce48d64452615bf737d8d6be2c8ffdbd6d
dcromer <cromer.dan@gmail.com>
2021-12-04 22:16:42 -0800

    Part 2

--- day5.rb
+++ day5.rb
@@ -6,8 +6,6 @@ input = File.open("input/day5.txt").readlines.map(&:strip).reject(&:empty?)
 size = 1000
 grid = (0..size).map { |_| (0..size).map { |_| 0 } }
 
-puts grid.map(&:join).join("\n")
-
 Point = Struct.new(:x,:y)
 input.each do |line|
     p1, _, p2 = line.split(" ")
@@ -24,6 +22,31 @@ input.each do |line|
     end
 end
 
-puts grid.map(&:join).join("\n")
+Helper.assert_equal 6225, grid.map { |row| row.select { |i| i >= 2}.count }.sum
 
-puts grid.map { |row| row.select { |i| i >= 2}.count }.sum
+# part 2
+grid = (0..size).map { |_| (0..size).map { |_| 0 } }
+
+input.each do |line|
+    p1, _, p2 = line.split(" ")
+    p1 = Point.new(*p1.split(",").map(&:to_i))
+    p2 = Point.new(*p2.split(",").map(&:to_i))
+
+    if p1.x == p2.x # vertical
+        Range.new(*[p1.y, p2.y].sort).to_a.each do |y|
+            grid[y][p1.x] += 1
+        end    
+    elsif p1.y == p2.y # horizontal
+        Range.new(*[p1.x, p2.x].sort).to_a.each do |x|
+            grid[p1.y][x] += 1
+        end
+    else
+        first, second = [p1,p2].sort_by(&:x)
+        rate = (second.y - first.y) / (second.x - first.x)
+        (second.x - first.x + 1).times do |i|
+            grid[first.y + i * rate][first.x + i] += 1
+        end
+    end
+end
+
+Helper.assert_equal 22116, grid.map { |row| row.select { |i| i >= 2}.count }.sum
