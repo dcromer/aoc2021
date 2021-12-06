60d39e1cd660b675b3a1b8b63ca4f8816ccc9140
dcromer <cromer.dan@gmail.com>
2021-12-05 22:39:40 -0800

    Day 6 part 2

--- day6.rb
+++ day6.rb
@@ -2,24 +2,23 @@ require './helper.rb'
 
 input = File.open("input/day6.txt").readlines.map(&:strip).reject(&:empty?)
 
-fish = input[0].split(",").map(&:to_i)
-
-days = 80
-
-days.times do |i|
-    new_fish = []
-    fish = fish.map do |f|
-        if f == 0
-            new_fish << 8
-            6
-        else
-            f - 1
-        end
-    end
-
-    fish.push(*new_fish)
+fish = input[0].split(",").map(&:to_i).each_with_object(Hash.new(0)) do |f, result|
+    result[f] += 1
 end
 
-puts fish.length
+def simulate_fish(f, days)
+    days.times do |i|
+        new_fish = Hash.new(0)
+        (0..7).each do |i|
+            new_fish[i] = f[i + 1]
+        end
+        new_fish[8] = f[0]
+        new_fish[6] += f[0]
+        f = new_fish
+    end
+    f.values.sum
+end
 
-# Part 1: 355386
+Helper.assert_equal 355386, simulate_fish(fish, 80)
+# Part 2
+Helper.assert_equal 1613415325809, simulate_fish(fish, 256)
