bb6b62b31f18e25da3d5950ee2b26dd07bb2c801
dcromer <cromer.dan@gmail.com>
2021-12-09 22:03:14 -0800

    Less iteration

--- day10.rb
+++ day10.rb
@@ -2,55 +2,35 @@ require './helper.rb'
 
 input = File.open("input/day10.txt").readlines.map(&:strip).reject(&:empty?)
 
-openers = "([{<".chars
-closers = ")]}>".chars
-invalid = []
-input.map do |input|
+OPENERS = "([{<".chars
+CLOSERS = ")]}>".chars
+PART_1_SCORE = [3,57,1197,25137]
+PART_2_SCORE = [1,2,3,4]
+
+invalid_score = 0
+scores = input.map do |input|
     stack = []
-    input.chars.each_with_index do |char, index|
-        if openers.include?(char)
+    input.chars.each do |char|
+        if OPENERS.include?(char)
             stack.push(char)
-        elsif char == closers[openers.index(stack.last)]
+        elsif char == CLOSERS[OPENERS.index(stack.last)]
             stack.pop
         else
-            invalid << char
+            invalid_score += PART_1_SCORE[CLOSERS.index(char)]
+            stack = []
             break
         end
     end
-end
 
-key = [3,57,1197,25137]
-score = invalid.map { |c| key[closers.index(c)] }.sum
-#puts score
-
-def get_stack(chars)
-    openers = "([{<".chars
-    closers = ")]}>".chars  
-    stack = []
-
-    chars.each_with_index do |char, index|
-        if openers.include?(char)
-            stack.push(char)
-        elsif char == closers[openers.index(stack.last)]
-            stack.pop
-        else
-            return nil
-        end
-    end
-
-    stack
-end
-
-key = [1,2,3,4]
-
-open_stacks = input.map do |line|
-    if stack = get_stack(line.chars)
+    if stack.any?
         score = 0
-        stack.reverse.map { |c| key[openers.index(c)] }.each do |i|
+        stack.reverse.map { |c| PART_2_SCORE[OPENERS.index(c)] }.each do |i|
             score = score * 5 + i
         end
         score
     end
 end.compact
 
-puts open_stacks.sort[open_stacks.length / 2]
+
+Helper.assert_equal 399153, invalid_score
+Helper.assert_equal 2995077699, scores.sort[scores.length / 2]
