672a28ca394dc86d8c694ae7dddb65dc052cd43a
dcromer <cromer.dan@gmail.com>
2021-12-09 21:33:17 -0800

    Day 10 part 2

--- day10.rb
+++ day10.rb
@@ -21,4 +21,36 @@ end
 
 key = [3,57,1197,25137]
 score = invalid.map { |c| key[closers.index(c)] }.sum
-puts score
+#puts score
+
+def get_stack(chars)
+    openers = "([{<".chars
+    closers = ")]}>".chars  
+    stack = []
+
+    chars.each_with_index do |char, index|
+        if openers.include?(char)
+            stack.push(char)
+        elsif char == closers[openers.index(stack.last)]
+            stack.pop
+        else
+            return nil
+        end
+    end
+
+    stack
+end
+
+key = [1,2,3,4]
+
+open_stacks = input.map do |line|
+    if stack = get_stack(line.chars)
+        score = 0
+        stack.reverse.map { |c| key[openers.index(c)] }.each do |i|
+            score = score * 5 + i
+        end
+        score
+    end
+end.compact
+
+puts open_stacks.sort[open_stacks.length / 2]
