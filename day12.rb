49d6749cc2f56d2d43cf0cd0c23aa95976987cd1
dcromer <cromer.dan@gmail.com>
2021-12-11 22:00:26 -0800

    Day 12 part 2

--- day12.rb
+++ day12.rb
@@ -12,7 +12,11 @@ class Node
     end
 
     def end?
-        @name == "end"
+      @name == "end"
+    end
+
+    def start?
+      @name == "start"
     end
 end
 
@@ -24,6 +28,7 @@ input.map do |line|
     nodes_by_name[start].edges << nodes_by_name[ending]
     nodes_by_name[ending].edges << nodes_by_name[start]
 end
+
 $paths = []
 def get_paths(start, current_path=[])
   current_path = current_path + [start.name]
@@ -40,6 +45,31 @@ def get_paths(start, current_path=[])
 end
 
 get_paths(nodes_by_name["start"])
+
+puts $paths.count
+#puts $paths.map { |p| p.join("-")}.join("\n")
+
+# Part 2
+$paths = []
+def get_paths2(start, current_path=[])
+  current_path = current_path + [start.name]
+
+  start.edges.select do |edge|
+      num_visits_by_node = current_path.group_by(&:itself).transform_values(&:length)
+      times_visited = num_visits_by_node[edge.name] || 0
+      visited_small_cave_twice = num_visits_by_node.select { |k, v| k == k.downcase && v > 1 }.any?
+
+      !edge.start? && (edge.large_cave || times_visited == 0 || (times_visited == 1 && !visited_small_cave_twice))
+  end.map do |edge|
+    if edge.end?
+      $paths << (current_path + [edge.name])
+    else
+      get_paths2(edge, current_path)
+    end
+  end
+end
+
+get_paths2(nodes_by_name["start"])
 
 puts $paths.count
 #puts $paths.map { |p| p.join("-")}.join("\n")
