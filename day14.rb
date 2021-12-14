1e945e7eceef6baeb69e161e9a9b4369d5c26c0d
dcromer <cromer.dan@gmail.com>
2021-12-13 21:58:53 -0800

    Refactor

--- day14.rb
+++ day14.rb
@@ -3,39 +3,23 @@ require './helper.rb'
 input = File.open("input/day14.txt").readlines.map(&:strip).reject(&:empty?)
 
 polymer_template = input.shift
-
-pair_insertion_rules = {}
-
-input.each do |line|
+pair_insertion_rules = input.each_with_object({}) do |line, result|
     match, insert_element = line.split("->").map(&:strip)
-    pair_insertion_rules[match] = insert_element
-end
-
-counts = {}
-for i in 0..(polymer_template.length - 2)
-    pair = polymer_template[i, 2]
-    counts[pair] ||= 0
-    counts[pair] += 1
+    result[match] = insert_element
 end
 
 def apply_rules(counts, pair_insertion_rules)
     matches = counts.keys & pair_insertion_rules.keys
 
-    new_counts = {}
+    new_counts = Hash.new(0)
     matches.each do |pair|
         to_insert = pair_insertion_rules[pair]
         count = counts[pair]
-        new_counts[pair[0] + to_insert] ||= 0
         new_counts[pair[0] + to_insert] += count
-        new_counts[to_insert + pair[1]] ||= 0
         new_counts[to_insert + pair[1]] += count
     end
 
     new_counts.merge(counts.except(*matches))
-end
-
-40.times do 
-    counts = apply_rules(counts, pair_insertion_rules)
 end
 
 def score(counts, initial)
@@ -48,4 +32,17 @@ def score(counts, initial)
     result[-1] - result[0]
 end
 
-puts score(counts, polymer_template[0])
+def solve_puzzle(polymer_template, pair_insertion_rules, iterations)
+    counts = polymer_template.chars.each_cons(2).map(&:join).group_by(&:itself).transform_values(&:length)
+
+    iterations.times do
+        counts = apply_rules(counts, pair_insertion_rules)
+    end
+
+    score(counts, polymer_template[0])
+end
+
+# Part 1
+Helper.assert_equal 3058, solve_puzzle(polymer_template, pair_insertion_rules, 10)
+# Part 2
+Helper.assert_equal 3447389044530, solve_puzzle(polymer_template, pair_insertion_rules, 40)
