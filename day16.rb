27a8b5590aae52ef2b3a548a74cf5dc64689f8eb
dcromer <cromer.dan@gmail.com>
2021-12-15 22:17:55 -0800

    Operator length type 0

--- day16.rb
+++ day16.rb
@@ -5,10 +5,25 @@ def convert_to_binary(hex_string)
     [hex_string].pack('H*').unpack('B*').first
 end
 
-LiteralValue = Struct.new(:version, :type_id, :literal_value)
+LiteralValue = Struct.new(:version, :literal_value)
+Operator = Struct.new(:version, :packets)
 
 def is_terminal_group?(bitstring)
   bitstring.start_with?('0')
+end
+
+def build_literal_value!(bitstring, version)
+  group = bitstring.slice!(0, 5)
+  groups = [group.slice(1, 4)]
+  length = 4
+  until is_terminal_group?(group)
+    group = bitstring.slice!(0, 5)
+    groups << group.slice(1, 4)
+
+    length += 4
+  end
+
+  return LiteralValue.new(version, groups.join.to_i(2)), length
 end
 
 def build_packets(bitstring)
@@ -19,22 +34,31 @@ def build_packets(bitstring)
       length = 6
       # Literal Value
       if type_id == 4
-        group = bitstring.slice!(0, 5)
-        groups = [group.slice(1, 4)]
-        length += 5
-        until is_terminal_group?(group)
-          group = bitstring.slice!(0, 5)
-          groups << group.slice(1, 4)
-
-          length += 5
-        end
-
+        value, length = build_literal_value!(bitstring, version)
+        e.yield value
         if trash = length % 4
           bitstring.slice!(0, length % 4)
         end
- 
-        e.yield LiteralValue.new(version, type_id, groups.join.to_i(2))
+      # Operator
+      else
+        length_type = bitstring.slice!(0, 1).to_i(2)
+        length += 1
+        if length_type == 0
+          total_length = bitstring.slice!(0, 15).to_i(2)
+          length += 15
+          packets = build_packets(bitstring.slice!(0, total_length))
+
+          e.yield Operator.new(version, packets.to_a)
+          if trash = length % 4
+            bitstring.slice!(0, length % 4)
+          end
+          bitstring.slice!(0, 4) # Mystery, part 2 maybe
+
+        else
+        end
       end
+
+
     end
   end
 end
@@ -45,6 +69,7 @@ Helper.assert_equal "110100101111111000101000", example_1_binary
 example_1_packet = build_packets(example_1_binary).first
 Helper.assert_equal 2021, example_1_packet.literal_value
 
-
-
-
+example_2_binary = "00111000000000000110111101000101001010010001001000000000"
+example_2_packet = build_packets(example_2_binary).to_a.first
+binding.irb
+Helper.assert_equal [10, 20], example_2_packet.packets.map(&:literal_value)
