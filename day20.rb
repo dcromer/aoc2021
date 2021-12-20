1f50e8ca2f221981a2efa4b31f6e7f0c0dfdaadb
dcromer <dan.cromer@outreach.io>
2021-12-19 23:51:44 -0700

    Small refactor

--- day20.rb
+++ day20.rb
@@ -1,7 +1,7 @@
 require './helper.rb'
 input = File.open("input/day20.txt").readlines.map(&:strip).reject(&:empty?)
 
-algorithm = input.shift.chars
+kernel = input.shift.chars
 
 image = input.map(&:chars)
 
@@ -22,10 +22,10 @@ def get_pixels(g, i, j)
     end.join
 end
 
-def pad(g, n, char=".")
+def pad(g, n, pad_char)
     new_length = g.length + 2 * n
-    blank_row = [char] * new_length
-    blank_cols = [char] * n
+    blank_row = [pad_char] * new_length
+    blank_cols = [pad_char] * n
 
     new_g = []
     n.times do
@@ -48,29 +48,32 @@ def map_pixel(g, a, i, j)
     a[index]
 end
 
-def enhance(image, algorithm)
-    #image = pad(image, 2)
+def convolve(image, kernel)
     new_image = image.length.times.map do |_|
         ["."] * image.length
     end
     Grid.all_with_index(image).each do |_, (i, j)|
-        new_image[i][j] = map_pixel(image, algorithm, j, i)
+        new_image[i][j] = map_pixel(image, kernel, j, i)
     end
     new_image
 end
 
-=begin
-2.times do
-    image = enhance(image, algorithm)
-end
-=end
-
-50.times do |i|
-    puts i
+# Part 1
+# This is sort of naive; instead of intelligently determining the padding for our convolution, we
+# cheat and realize that the kernel always maps 0's to 1's and vice versa.
+2.times do |i|
     char = i % 2 == 0 ? "." : "#"
     image = pad(image, 2, char)
-    image = enhance(image, algorithm)
+    image = convolve(image, kernel)
 end
 
-puts Grid.all(image).select { |p| p == "#" }.count
+Helper.assert_equal 5225, Grid.all(image).select { |p| p == "#" }.count
 
+48.times do |i|
+    puts "Step: #{i}"
+    char = i % 2 == 0 ? "." : "#"
+    image = pad(image, 2, char)
+    image = convolve(image, kernel)
+end
+
+Helper.assert_equal 18131, Grid.all(image).select { |p| p == "#" }.count
